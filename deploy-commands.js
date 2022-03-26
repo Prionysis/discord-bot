5b128f56abbb0f11784d83796e0538f0511b2550
Prionysis <pasenidis.dionisios@gmail.com>
2022-03-26 19:40:19 +0200

    refactor: remove unused token import

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 const fs = require("fs")
 const { REST } = require("@discordjs/rest")
 const { Routes } = require("discord-api-types/v9")
-const { clientId, guildId, token } = require("./config.json")
+const { clientId, guildId } = require("./config.json")
 
 const commands = []
 const commandFiles = fs
